e463186a930b8b20a943daff4344e16bfeda4b73
niamipek <161831080+niamipek@users.noreply.github.com>
2024-05-07 23:16:27 +0700

    search and filter(beta)

--- Fontend/filter/searchProduct.php
+++ Fontend/filter/searchProduct.php
@@ -8,84 +8,45 @@ $p_name = $_POST['p_name'];
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+$sql = "SELECT * FROM product WHERE p_name LIKE '%$p_name%'";
+$result = $conn->query($sql);
 
-// Kiểm tra xem người dùng có nhập từ khóa "deer" không
-if ($p_name === "deer") {
-    // Thực hiện hành động khi người dùng nhập từ khóa "deer"
-    // Ví dụ: hiển thị một tin nhắn thông báo
-    $sql = "SELECT * FROM product WHERE p_name = '$p_name'";
-    $result = $conn->query($sql);
-
-    if ($result->num_rows > 0) {
-        while ($row = $result->fetch_assoc()) {
-            echo '
-            <div id="' . $row['p_idfake'] . '" class="col-sm-6 col-md-4 col-lg-3 p-b-35 isotope-item toy canbanghinh' . $row['p_id'] .' " >
-                <div class="block2">
-                    <div class="block2-pic hov-img0 box' . $row['p_id'] .' heeee">
-                        <img  src="images/' . $row['p_image'] . '" alt="IMG-PRODUCT">
-                        <div>
-                            <i id="cart' . $row['p_id'] . '" class="fa-duotone fa-basket-shopping-simple hand-icon icon icon1 cart1" style="--fa-primary-color: #d27014; --fa-secondary-color: #d27014; visibility: hidden;"></i>
-                            <i id="love' . $row['p_id'] . '" class="fa-light fa-heart hand-icon icon icon1" style="color: #ea931a; visibility: hidden;"></i>
-                            <i id="view' . $row['p_id'] . '" class="fa-solid fa-eye hand-icon icon" style="visibility: hidden;"></i>
-                        </div>
+// Kiểm tra xem có sản phẩm nào được tìm thấy không
+if ($result->num_rows > 0) {
+    // Lặp qua các hàng kết quả
+    while($row = $result->fetch_assoc()) {
+        $idFake = $row['p_idfake'];
+        $image = $row['p_image'];
+        $name = $row['p_name'];
+        $type = $row['p_type'];
+        $price = $row['p_price'];
+        ?>
+        <div class="col-sm-6 col-md-4 col-lg-3 p-b-35 isotope-item toy">
+            <div class="block2">
+                <div id="<?= $idFake ?>" class="block2-pic hov-img0" style="border: 0.1px dashed #000; border-radius: 50px;">
+                    <img src="images/<?= $image ?>" alt="IMG-PRODUCT">
+                </div>
+                <div class="block2-txt flex-w flex-t p-t-14">
+                    <div class="block2-txt-child1 flex-col-l">
+                        <a href="product-detail.html" class="stext-104 cl4 hov-cl1 trans-04 js-name-b2 p-b-6"><?= $name ?></a>
+                        <p class="stext-104 cl4 hov-cl1 trans-04 js-name-b2 p-b-6 text1"><?= $type ?></p>
+                        <span class="stext-105 cl3 price">$<?= $price ?></span>
                     </div>
-                    <div class="block2-txt flex-w flex-t p-t-14">
-                        <div class="block2-txt-child1 flex-col-l">
-                            <a href="product-detail.html" class="stext-104 cl4 hov-cl1 trans-04 js-name-b2 p-b-6 text">
-                                ' . $row['p_name'] . '
-                            </a>
-                            <p class="stext-104 cl4 hov-cl1 trans-04 js-name-b2 p-b-6 text1">
-                                ' . $row['p_type'] . '
-                            </p>
-                            <span class="stext-105 cl3 price">
-                                ' . $row['p_price'] . '
-                            </span>
-                        </div>
-                        <div class="block2-txt-child2 flex-r p-t-3"></div>
+                    <div class="block2-txt-child2 flex-r p-t-3">
+                        <a href="#" class="btn-addwish-b2 dis-block pos-relative js-addwish-b2">
+                            <img class="icon-heart1 dis-block trans-04" src="images/icons/icon-heart-01.png" alt="ICON">
+                            <img class="icon-heart2 dis-block trans-04 ab-t-l" src="images/icons/icon-heart-02.png" alt="ICON">
+                        </a>
                     </div>
                 </div>
-            </div>';
-        }
-    } else {
-        echo "<p>Không tìm thấy sản phẩm.</p>";
+            </div>
+        </div>
+        <?php
     }
 } else {
-    // Truy vấn dữ liệu từ bảng product
-    $sql = "SELECT * FROM product WHERE p_name = '$p_name'";
-    $result = $conn->query($sql);
-
-    if ($result->num_rows > 0) {
-        while ($row = $result->fetch_assoc()) {
-            echo '
-            <div id="' . $row['p_idfake'] . '" class="col-sm-6 col-md-4 col-lg-3 p-b-35 isotope-item toy canbanghinh' . $row['p_id'] .' " >
-                <div class="block2">
-                    <div class="block2-pic hov-img0 box' . $row['p_id'] .'">
-                        <img  src="images/' . $row['p_image'] . '" alt="IMG-PRODUCT">
-                        <div>
-                            <i id="cart' . $row['p_id'] . '" class="fa-duotone fa-basket-shopping-simple hand-icon icon icon1 cart1" style="--fa-primary-color: #d27014; --fa-secondary-color: #d27014; visibility: hidden;"></i>
-                            <i id="love' . $row['p_id'] . '" class="fa-light fa-heart hand-icon icon icon1" style="color: #ea931a; visibility: hidden;"></i>
-                            <i id="view' . $row['p_id'] . '" class="fa-solid fa-eye hand-icon icon" style="visibility: hidden;"></i>
-                        </div>
-                    </div>
-                    <div class="block2-txt flex-w flex-t p-t-14">
-                        <div class="block2-txt-child1 flex-col-l">
-                            <a href="product-detail.html" class="stext-104 cl4 hov-cl1 trans-04 js-name-b2 p-b-6 text">
-                                ' . $row['p_name'] . '
-                            </a>
-                            <p class="stext-104 cl4 hov-cl1 trans-04 js-name-b2 p-b-6 text1">
-                                ' . $row['p_type'] . '
-                            </p>
-                            <span class="stext-105 cl3 price">
-                                ' . $row['p_price'] . '
-                            </span>
-                        </div>
-                        <div class="block2-txt-child2 flex-r p-t-3"></div>
-                    </div>
-                </div>
-            </div>';
-        }
-    } else {
-        echo "<p>Không tìm thấy sản phẩm.</p>";
-    }
+    // Hiển thị thông báo nếu không tìm thấy sản phẩm
+    echo "Không tìm thấy sản phẩm phù hợp";
 }
+// Đóng kết nối cơ sở dữ liệu
+$conn->close();
 ?>
