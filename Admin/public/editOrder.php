10d8c0c671712e590efa49ee3fb9e3801b7374d7
niamipek <161831080+niamipek@users.noreply.github.com>
2024-05-11 02:24:27 +0700

    update manageOder

--- Admin/public/editOrder.php
+++ Admin/public/editOrder.php
@@ -124,7 +124,7 @@ if(isset($_GET['o_id']) && is_numeric($_GET['o_id'])) {
                   d="M15 15l-2 5L9 9l11 4-5 2zm0 0l5 5M7.188 2.239l.777 2.897M5.136 7.965l-2.898-.777M13.95 4.05l-2.122 2.122m-5.657 5.656l-2.12 2.122">
                 </path>
               </svg>
-              <span class="ml-4">Manage Order</span>
+              <span class="ml-4">Manage Order </span>
             </a>
           </li>
           <li class="relative px-6 py-3">
